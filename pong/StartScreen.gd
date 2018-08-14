448245b3426afe1043a40fd0f160d40832a40a2a
Sergey Manvelov <smanvelov@gmail.com>
2018-08-14 19:36:21 +0300

    iSMPong update

--- pong/StartScreen.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-# func _ready():
-func _on_Play_pressed():
-	get_tree().change_scene("res://mainscreen.tscn")
-
-func _on_Settings_pressed():
-	get_tree().change_scene("res://settings.tscn")
