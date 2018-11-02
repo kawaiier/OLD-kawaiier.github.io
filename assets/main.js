d873390754f7e2d0fae2a331db0fca01e85089e7
Sergey Manvelov <smanvelov@gmail.com>
2018-11-02 13:40:19 +0300

    Made a few changes to css and html of the webpage

--- assets/main.js
+++ assets/main.js
@@ -9,17 +9,17 @@ ourRequest.onload = function() {
 ourRequest.send();
 
 function renderHTML(data) {
-  var htmlString = "<em>Цитата дня. ";
+  var htmlString = "«";
 
   var randnum1 = Math.floor((Math.random() * data.length) + 0);
   var randnum2 = Math.floor((Math.random() * data.length) + 0);
 
   if (randnum1 == randnum2) {
-    htmlString += data[randnum1].name + ": «" + data[randnum2].qoute + "». <strong>БИНГО!</strong>";
+    htmlString += data[randnum1].name + ". " + data[randnum2].quote + "» — <b>БИНГО!</b>";
   }
   else {
-    htmlString += data[randnum1].name + ": «" + data[randnum2].qoute + "».";
+    htmlString += data[randnum1].name + ". " + data[randnum2].quote + "»";
   }
 
-  quotesContainer.insertAdjacentHTML('beforeend', htmlString + "</em>");
+  quotesContainer.insertAdjacentHTML('beforeend', htmlString);
 };
