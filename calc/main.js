313a784fa2f0ec4d2c56019e8505f58fc0a2a233
Kawaiier <smanvelov@gmail.com>
2020-04-01 22:01:31 +0400

    Window Calc added
    Former-commit-id: 06aea08e14816534f6365cdd5840dbefe851e224

---
+++ calc/main.js
@@ -0,0 +1,22 @@
+const resultsList = document.getElementById("results");
+const usp = new URLSearchParams(window.location.search);
+
+const totalWindows = usp.get("totalWindows");
+const litWindows = usp.get("litWindows");
+const unlitWindows = totalWindows - litWindows;
+const unlitWindowsPercentage = litWindows / totalWindows * 100
+
+if (unlitWindowsPercentage == 100){
+    resultsList.append(`Все окошки горят! :)`)
+} else if (unlitWindowsPercentage == 0) {
+    resultsList.append(`Ни одно окошко не горит :'(`)
+} else {
+    resultsList.append(`Не горит ${unlitWindows} окошек, то есть ${unlitWindowsPercentage}%`)
+}
+
+
+// for (const [key, value] of usp){
+//     resultsList.append(`${key} => ${value}`);
+//     resultsList.append(document.createElement("br"));
+// }
+
