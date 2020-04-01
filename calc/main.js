759fdd2525cc700c1f0cb777144b95de728bd21e
Kawaiier <smanvelov@gmail.com>
2020-04-01 22:09:27 +0400

    Window Calc minor update
    Former-commit-id: 9059e88c513ac494ce14cee0a1f00e2583a208e6

--- calc/main.js
+++ calc/main.js
@@ -4,15 +4,15 @@ const usp = new URLSearchParams(window.location.search);
 const totalWindows = usp.get("totalWindows");
 const litWindows = usp.get("litWindows");
 const unlitWindows = totalWindows - litWindows;
-const unlitWindowsPercentage = Math.round(litWindows / totalWindows * 100)
+const unlitWindowsPercentage = Math.round(litWindows / totalWindows * 100);
 
 if (unlitWindowsPercentage == 100){
-    resultsList.append(`Все окошки горят! :)`)
+    resultsList.append(`Все окошки горят! :)`);
 } else if (unlitWindowsPercentage == 0) {
-    resultsList.append(`Ни одно окошко не горит :'(`)
+    resultsList.append(`Ни одно окошко не горит :'(`);
 } else {
-    resultsList.append(`Не горит ${unlitWindows} окошек, то есть ${unlitWindowsPercentage}%`)
-}
+    resultsList.append(`Не горит ${unlitWindows} окошек, то есть ${unlitWindowsPercentage}%`);
+};
 
 
 // for (const [key, value] of usp){
