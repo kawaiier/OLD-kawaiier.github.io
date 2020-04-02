3f1df9a77194c38832dd45a6f0a32c45c6bd4224
Kawaiier <smanvelov@gmail.com>
2020-04-02 22:18:16 +0400

    Fixed bug when all windows are lit
    Former-commit-id: 359346d97cd20504dcda6fa2be29bd3972a1804e

--- calc/main.js
+++ calc/main.js
@@ -13,9 +13,9 @@ const unlitWindowsPercentage = Math.round(100- litWindows / totalWindows * 100);
 
 
 if (unlitWindowsPercentage == 100){
-    resultsList.append(`Все окошки горят! :)`);
-} else if (unlitWindowsPercentage == 0) {
     resultsList.append(`Ни одно окошко не горит :'(`);
+} else if (unlitWindowsPercentage == 0) {
+    resultsList.append(`Все окошки горят! :)`);
 } else {
     resultsList.append(`Не горит ${unlitWindows} окошек, то есть ${unlitWindowsPercentage}%`);
 };    
