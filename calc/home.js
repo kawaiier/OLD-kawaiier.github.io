e62787a5b100bc5606a3b306cb1af605a5a4c439
Kawaiier <smanvelov@gmail.com>
2020-04-03 10:20:42 +0400

    Beautified Code <3
    Former-commit-id: 1d8534aa7b1e2d5b7106de3188e64c73dd9818d3

--- calc/home.js
+++ calc/home.js
@@ -9,20 +9,20 @@ const floorsAllowed = 30;
 let floorWindows = usp.get("floorWindows");
 let floors = usp.get("floors");
 
-function max_limit_check(){
-  if (floorWindows > floorWindowsAllowed){
-    floorWindows = floorWindowsAllowed;
-  }
-  
-  if (floors > floorsAllowed){
-    floors = floorsAllowed;
-  }  
+function max_limit_check() {
+    if (floorWindows > floorWindowsAllowed) {
+        floorWindows = floorWindowsAllowed;
+    }
+
+    if (floors > floorsAllowed) {
+        floors = floorsAllowed;
+    }
 }
 
 max_limit_check();
 
 const totalWindows = floorWindows * floors;
-  localStorage.setItem("totalWindows", totalWindows);
+localStorage.setItem("totalWindows", totalWindows);
 
 // if (localStorage.getItem("floorWindowsCache") == null || localStorage.getItem("floorsCache") == null){
 //   const totalWindows = floorWindows * floors;
@@ -30,66 +30,63 @@ const totalWindows = floorWindows * floors;
 // }
 
 if (localStorage.getItem("floorWindowsCache") == null) {
-  localStorage.setItem("floorWindowsCache", floorWindows);
+    localStorage.setItem("floorWindowsCache", floorWindows);
 }
 if (localStorage.getItem("floorsCache") == null) {
-  localStorage.setItem("floorsCache", floors);
+    localStorage.setItem("floorsCache", floors);
 }
 
-homeList.append(`Всего в доме ${totalWindows} окон!`);
+homeList.append(`Всего в доме ${totalWindows} окошек!`);
 homeList.innerHTML += "<br>";
 homeList.innerHTML += "<br>";
-
-//totalFlatsList.append(totalWindows);
 
 function generate_table() {
     // get the reference for the body
     var body = document.getElementById("home");
-  
+
     // creates a <table> element and a <tbody> element
     var tbl = document.createElement("table");
     var tblBody = document.createElement("tbody");
-  
+
     // creating all cells
     for (var i = 0; i < floors; i++) {
-      // creates a table row
-      var row = document.createElement("tr");
-  
-      for (var j = 0; j < floorWindows; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        var cell = document.createElement("td");
-        cell.classList.add("checkbox-style");
-        var checkbox = document.createElement('input');
-        checkbox.type = "checkbox";
-        checkbox.name = "flat";
-        checkbox.value = 1;
-        checkbox.label = "";
-        checkbox.id = j;
-        //var cellText = document.createTextNode("cell in row "+i+", column "+j);
-        //cell.appendChild(cellText);
-        cell.appendChild(checkbox);
-        row.appendChild(cell);
-      }
-  
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
+        // creates a table row
+        var row = document.createElement("tr");
+
+        for (var j = 0; j < floorWindows; j++) {
+            // Create a <td> element and a text node, make the text
+            // node the contents of the <td>, and put the <td> at
+            // the end of the table row
+            var cell = document.createElement("td");
+            cell.classList.add("checkbox-style");
+            var checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+            checkbox.name = "flat";
+            checkbox.value = 1;
+            checkbox.label = "";
+            checkbox.id = j;
+            //var cellText = document.createTextNode("cell in row "+i+", column "+j);
+            //cell.appendChild(cellText);
+            cell.appendChild(checkbox);
+            row.appendChild(cell);
+        }
+
+        // add the row to the end of the table body
+        tblBody.appendChild(row);
     }
-  
+
     // put the <tbody> in the <table>
     tbl.appendChild(tblBody);
     // appends <table> into <body>
     body.appendChild(tbl);
     // sets the border attribute of tbl to 2;
     tbl.setAttribute("border", "2");
-  }
+}
 
-  if (floors != null && floorWindows != null){
+if (floors != null && floorWindows != null) {
     generate_table();
-  } else {
+} else {
     floors = localStorage.getItem("floorsCache");
     floorWindows = localStorage.getItem("floorWindowsCache");
     generate_table();
-  }
-  
+}
--- calc/main.js
+++ calc/main.js
@@ -4,7 +4,7 @@ const usp = new URLSearchParams(window.location.search);
 const totalWindows = localStorage.getItem("totalWindows");
 const litWindows = usp.getAll("flat").length;
 const unlitWindows = totalWindows - litWindows;
-const unlitWindowsPercentage = Math.round(100- litWindows / totalWindows * 100);
+const unlitWindowsPercentage = Math.round(100 - litWindows / totalWindows * 100);
 
 // const totalWindows = usp.get("totalWindows");
 // const litWindows = usp.get("litWindows");
@@ -12,17 +12,16 @@ const unlitWindowsPercentage = Math.round(100- litWindows / totalWindows * 100);
 // const unlitWindowsPercentage = Math.round(litWindows / totalWindows * 100);
 
 
-if (unlitWindowsPercentage == 100){
+if (unlitWindowsPercentage == 100) {
     resultsList.append(`Ни одно окошко не горит :'(`);
 } else if (unlitWindowsPercentage == 0) {
     resultsList.append(`Все окошки горят! :)`);
 } else {
     resultsList.append(`Не горит ${unlitWindows} окошек, то есть ${unlitWindowsPercentage}%`);
-};    
+};
 
 
 // for (const [key, value] of usp){
 //     resultsList.append(`${key} => ${value}`);
 //     resultsList.append(document.createElement("br"));
 // }
-
