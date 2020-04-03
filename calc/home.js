b6a2d371462ed04ea204fdcaf75b34d0585e0f46
Kawaiier <smanvelov@gmail.com>
2020-04-03 11:59:50 +0400

    Update home.js
    Former-commit-id: 63e2663a2280ab55a798986010ff7e65fcba1333

--- calc/home.js
+++ calc/home.js
@@ -1,3 +1,12 @@
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+console.log(datetime);
+
 const usp = new URLSearchParams(window.location.search);
 const homeList = document.getElementById("home");
 
